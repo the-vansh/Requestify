37f39dbbdfd39c0ce8522d7f1d4358f0f29f0a6e
the-vansh <126165244+the-vansh@users.noreply.github.com>
2025-06-05 17:44:58 +0530

    Update dbconnection.js

--- Backend/dbconnection.js
+++ Backend/dbconnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Requestify');
+    await mongoose.connect('mongodb+srv://vs3943943:7K5YucFOzUIsVWEX@requestify.0clpe24.mongodb.net/');
   } catch (error) {
     process.exit(1);
   }
